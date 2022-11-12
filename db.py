9324badb709f8ad8466e7758ed18df30752d7f03
Arnav Kumar <amkumar@princeton.edu>
2022-11-12 13:46:32 -0500

    Gunicorn

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_pymongo import pymongo
-from app import app
 
 CONNECTION_STRING = "mongodb+srv://amkumar:amkumar@rekindl.ixsatdr.mongodb.net/?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING)
