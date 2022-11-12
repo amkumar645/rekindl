d401e3881cdf6b41fdb54a28d6816746440bab2d
Arnav Kumar <amkumar@princeton.edu>
2022-11-12 15:00:54 -0500

    Update working

--- app.py
+++ app.py
@@ -29,17 +29,17 @@ def get_all_users():
 def add_user():
     data = request.form
     interests = data['interests'].split(",")
-    hobbies = data['hobbies'].split(",")
     db.user_collection.insert_one({
         "username": data['username'],
         "password": data['password'],
         "name": data['name'],
+        "location": data['location'],
+        "birthday": data['birthday'],
         "interests": interests,
-        "hobbies": hobbies,
         "friends": [],
         "relationship": [],
         "last_time_friend_seen": [],
-        "reminder_for_friend": []
+        "reminder_for_friend": [],
     })
     return "Added User!"
 
@@ -47,22 +47,28 @@ def add_user():
 @app.route('/users/update/<username>', methods=['POST'])
 def update_user(username):
     data = request.form
-    db.user_collection.update_one(
-    {
-        'username': username
-    },
-    {
-        "username": username,
-        "password": data['password'],
-        "name": data['name'],
-        "interests": data['interests'],
-        "hobbies": data['hobbies'],
-        "friends": data['friends'],
-        "relationship": [],
-        "last_time_friend_seen": data['last_time_friend_seen'],
-        "reminder_for_friend": data['reminder_for_friend']
-    })
-    return "Added User!"
+    interests = data['interests'].split(",")
+    friends = data['friends'].split("_")
+    relationship = data['relationship'].split("_")
+    last_time_friend_seen = data['last_time_friend_seen'].split("_")
+    reminder_for_friend = data['reminder_for_friend'].split("_")
+    filter = {"username": username}
+    new_vals = { "$set":
+        {
+            "username": username,
+            "password": data['password'],
+            "name": data['name'],
+            "location": data['location'],
+            "birthday": data['birthday'],
+            "interests": interests,
+            "friends": friends,
+            "relationship": relationship,
+            "last_time_friend_seen": last_time_friend_seen,
+            "reminder_for_friend": reminder_for_friend
+        }
+    }
+    db.user_collection.update_one(filter, new_vals)
+    return "Updated User!"
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(port=8000, debug=True)
--- helper.py
+++ helper.py
@@ -4,16 +4,23 @@ def list_to_string(list):
         s += item + ','
     return s[:-1]
 
+def list_to_string_friends(list):
+    s = ''
+    for item in list:
+        s += item + '_'
+    return s[:-1]
+
 def get_json_for_user(user):
     user_json = {
         'username': user['username'],
         'password': user['password'],
         'name': user['name'],
+        "location": user['location'],
+        "birthday": user['birthday'],
         'interests': list_to_string(user['interests']),
-        'hobbies': list_to_string(user['hobbies']),
-        'friends': list_to_string(user['friends']),
-        "relationship": list_to_string(user['relationship']),
-        'last_time_friend_seen': list_to_string(user['last_time_friend_seen']),
-        'reminder_for_friend': list_to_string(user['reminder_for_friend'])
+        'friends': list_to_string_friends(user['friends']),
+        "relationship": list_to_string_friends(user['relationship']),
+        'last_time_friend_seen': list_to_string_friends(user['last_time_friend_seen']),
+        'reminder_for_friend': list_to_string_friends(user['reminder_for_friend']),
     }
     return user_json
