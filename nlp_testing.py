34734530b852a436ccd888011ec4cab076454408
Arnav Kumar <amkumar@princeton.edu>
2022-11-12 18:15:44 -0500

    pickle fix

--- nlp_testing.py
+++ nlp_testing.py
@@ -2,8 +2,10 @@ import spacy
 import numpy as np
 import pickle
 import os
+import pathlib
+import platform
 
-nlp = pickle.load(open(os.path.join('./nlp_md.pkl'), 'rb'))
+nlp = pickle.load(open('./nlp_md.pkl', 'rb'))
 
 # testing for threshold value
 # words we want the model to report as similar
@@ -14,7 +16,8 @@ while line != '':
     words = line.split(', ')
     first_word = nlp(words[0])
     sims = [first_word.similarity(nlp(x)) for x in words[1:]]
-    similar_sims.append(sims)
+    for num in sims:
+        similar_sims.append(num)
     line = file.readline()
 print(similar_sims)
 
